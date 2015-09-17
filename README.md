d4de237a7da755dccce27ab26b4ade09cbf04a5c
juankparis <juan.k.paris@gmail.com>
2015-09-17 16:11:51 -0500

    arreglo de animacion de el nav

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # portafolioj creado y diceñado por juan carlos alfonso cruz
+
+<!-- <ul class="Menu-lista">
+	<li class="Menu-item">
+		<a class="Menu-link" href="">inicio</a>
+		<div class="Menu-animacion"></div>
+	</li>
+</ul>
+ -->
+	&-link
+		box-sizing border-box
+		
+		
+		text-decoration none
+		z-index: 2
+
+	/*&-link:hover + .Menu-animacion
+		transition all 0.3s linear
+		left 0
+	&-animacion
+		position absolute
+		background azul_baseObscuro
+		top 0
+		left -210px
+		width 210px
+		height 53px
+	*/
--- js/dinamismo.js
+++ js/dinamismo.js
@@ -12,4 +12,32 @@ $(function(){
 		$("#Header-buttonCerrar").removeClass("mostrar");
 	}
 
+	var parent, Menulink, diametro, x, y;
+	$(".Menu-link").click(function(e){
+		parent = $(this).parent();
+	//create .ink element if it doesn't exist
+	if(parent.find(".Menu-animac").length == 0){
+		parent.prepend("<span class='Menu-animac'></span>");
+	}
+		
+	Menulink = parent.find(".Menu-animac");
+	//incase of quick double clicks stop the previous animation
+	Menulink.removeClass("Menu-animate");
+	
+	//set size of .ink
+	if(!Menulink.height() && !Menulink.width())
+	{
+		//use parent's width or height whichever is larger for the diameter to make a circle which can cover the entire element.
+		diametro = Math.max(parent.outerWidth(), parent.outerHeight());
+		Menulink.css({height: diametro, width: diametro});
+	}
+	
+	//get click coordinates
+	//logic = click coordinates relative to page - parent's position relative to page - half of self height/width to make it controllable from the center;
+	x = e.pageX - parent.offset().left - Menulink.width()/2;
+	y = e.pageY - parent.offset().top - Menulink.height()/2;
+	
+	//set the position and add class .animate
+	Menulink.css({top: y+'px', left: x+'px'}).addClass("Menu-animate");
+	});
 });
