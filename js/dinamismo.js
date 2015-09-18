b1ec9f5e04db6cc9ba65d3870eec32ffb32c313b
juankparis <juan.k.paris@gmail.com>
2015-09-17 20:01:00 -0500

    arreglo de function animate

--- js/dinamismo.js
+++ js/dinamismo.js
@@ -12,32 +12,33 @@ $(function(){
 		$("#Header-buttonCerrar").removeClass("mostrar");
 	}
 
-	var parent, Menulink, diametro, x, y;
+	//genera el el efecto de ondulacion en el sistema de navegagion
+	var pariente, Menulink, diametro, x, y;
+
 	$(".Menu-link").click(function(e){
-		parent = $(this).parent();
-	//create .ink element if it doesn't exist
-	if(parent.find(".Menu-animac").length == 0){
-		parent.prepend("<span class='Menu-animac'></span>");
+		pariente = $(this).parent();
+	//si el elemento span no exsiste agregelo con la clase .Menu-animac
+	if(pariente.find(".Menu-animac").length == 0){
+		pariente.prepend("<span class='Menu-animac'></span>");
 	}
-		
-	Menulink = parent.find(".Menu-animac");
-	//incase of quick double clicks stop the previous animation
+	//buca en padre del elemento .Menulink
+	Menulink = pariente.find(".Menu-animac");
+	//en caso de otro clicks detener la anterior animacion
 	Menulink.removeClass("Menu-animate");
 	
-	//set size of .ink
-	if(!Menulink.height() && !Menulink.width())
-	{
-		//use parent's width or height whichever is larger for the diameter to make a circle which can cover the entire element.
-		diametro = Math.max(parent.outerWidth(), parent.outerHeight());
+	
+	if(!Menulink.height() && !Menulink.width()){
+		//utilizar el ancho y altura que sea mayor al diametro para hecer un circulo que pueda cubrir todo el elemento
+		diametro = Math.max(pariente.outerWidth(), pariente.outerHeight());
 		Menulink.css({height: diametro, width: diametro});
 	}
 	
-	//get click coordinates
-	//logic = click coordinates relative to page - parent's position relative to page - half of self height/width to make it controllable from the center;
-	x = e.pageX - parent.offset().left - Menulink.width()/2;
-	y = e.pageY - parent.offset().top - Menulink.height()/2;
+	//se obtiene las cordenadas del los clicks  
+	//click coordina relativa a la página de los padres en posición relativa a la página, un medio de auto altura y anchura para que sea controlable desde el centro
+	x = e.pageX - pariente.offset().left - Menulink.width()/2;
+	y = e.pageY - pariente.offset().top - Menulink.height()/2;
 	
-	//set the position and add class .animate
+	//pasa la posicion en (y,x) y anñade la clase Menuanimate (se la pasa al Menu link por eso es necesario poner el position absolute en el)
 	Menulink.css({top: y+'px', left: x+'px'}).addClass("Menu-animate");
 	});
 });
